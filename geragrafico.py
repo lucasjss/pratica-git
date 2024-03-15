7d900a975ebb71e66218fdf6a6abecdcbabaa0cc
lucasjss <lucas.aj2005@gmail.com>
2024-03-15 09:41:16 -0300

    Inserindo labels para os eixos horizontal e vertical

--- geragrafico.py
+++ geragrafico.py
@@ -7,6 +7,8 @@ def main():
     valores = leitor.getValores()
     print(valores)
     plt.plot(valores)
+    plt.ylabel('Valores de entrada')
+    plt.xlabel('Amostragem')
     plt.show()
 
 main()
